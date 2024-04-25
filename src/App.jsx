64bc9a172ca6dff0db23c4d0dde2f51c6202b0df
Dmitriy_Kondratiev <d.kondratiev1@gmail.com>
2024-04-25 11:17:50 +0300

    add solution 3

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,20 @@ const products = productsFromServer.map((product) => {
 
 export const App = () => {
   const [activeUser, setActiveUser] = useState('All');
+  const [query, setQuery] = useState('');
 
   let visibleProducts = [...products];
 
   if (activeUser !== 'All') {
     visibleProducts = visibleProducts
       .filter(product => product.user.name === activeUser);
+  }
+
+  if (query) {
+    visibleProducts = visibleProducts
+      .filter(product => product.name
+        .toLocaleLowerCase()
+        .includes(query.trim().toLocaleLowerCase()));
   }
 
   return (
@@ -65,20 +73,25 @@ export const App = () => {
                   type="text"
                   className="input"
                   placeholder="Search"
-                  value="qwe"
+                  value={query}
+                  onChange={e => setQuery(e.target.value)}
                 />
 
 
                 <span className="icon is-left">
                   <i className="fas fa-search" aria-hidden="true" />
                 </span>
+
                 <span className="icon is-right">
                   {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-                  <button
-                    data-cy="ClearButton"
-                    type="button"
-                    className="delete"
-                  />
+                  {query && (
+                    <button
+                      data-cy="ClearButton"
+                      type="button"
+                      className="delete"
+                      onClick={() => setQuery('')}
+                    />
+                  )}
                 </span>
               </p>
             </div>
